309835c747db77f34112ff1f551f944f41ddb8ff
riokai <489272441@qq.com>
2015-06-25 09:43:16 +0800

    新增 前端功能逻辑

---
+++ public/js/chat.js
@@ -0,0 +1,114 @@
+var Chat = function ( socket ) {
+  this.socket = socket;
+};
+
+Chat.prototype.sendMessage = function( room, text ) {
+  this.socket.emit( 'message', {
+    room: room,
+    text: text
+  });
+};
+
+Chat.prototype.changeRoom = function ( room ) {
+  this.socket.emit( 'join', {
+    newRoom: room
+  });
+};
+
+// 处理聊天命令
+Chat.prototype.processCommand = function( command ) {
+  var words = command.split(' ');
+  var message = false;
+  var room;
+  var name;
+
+  command = words[0]
+                  .substring( 1, words[0].length )
+                  .toLowerCase();
+
+  switch ( command ) {
+    case 'join':
+      words.shift();
+      room = words.join(' ');
+      this.changeRoom( room );
+      break;
+
+    case 'nick':
+      words.shift();
+      name = words.join(' ');
+      this.socket.emit( 'nameAttempt', name );
+      break;
+
+    default:
+      message = '未识别的命令！';
+      break;
+  }
+
+  return message;
+};
+
+var socket = io.connect();
+
+$(document).ready( function() {
+  var chatApp = new Chat( socket );
+
+  // 显示更名尝试结果
+  socket.on( 'nameResult', function( result ) {
+    var message;
+
+    if ( result.success ) {
+      message = '更名成功，你现在的昵称为' + result.name + '.';
+    } else {
+      message = result.message;
+    }
+
+    $('#message').append( divSystemContentElement( message ) );
+  });
+
+  // 显示房间变更结果
+  socket.on( 'joinResult', function( result ) {
+    $('#room').text( result.room );
+    $('#messages').append( divSystemContentElement( '房间变更。') );
+  } );
+
+  // 显示接收到的消息
+  socket.on( 'message', function( message ) {
+    var newElement = $('<div></div>').text( message.text );
+
+    $('#messages').append( newElement );
+  });
+
+  // 显示可用房间列表
+  socket.on( 'rooms', function( rooms ) {
+    $('#room-list').empty();
+
+    for (var room in rooms) {
+      room = room.substring( 1, room.length );
+
+      if ( room !== '' ) {
+        $('#room-list').append( divEscapedContentElement( room ) );
+      }
+    }
+    // 点击房间名切换房间
+    $('#room-list div').click( function() {
+      chatApp.processCommand( '/join ' + $( this ).text() );
+
+      $('#send-message').focus(); 
+    } );
+
+    // 定时请求可用房间列表
+    setInterval( function() {
+      socket.emit( 'rooms' );
+    }, 1000);
+
+    $('#send-message').focus();
+
+    $('#send-form').submit( function() {
+      processUserInput( chatApp, socket );
+
+      return false;
+    });
+
+  } );
+
+} );
---
+++ public/js/chat_ui.js
@@ -0,0 +1,28 @@
+var divEscapedContentElement = function( message ) {
+  return $('<div></div>').text( message );
+};
+
+var divSystemContentElement = function( message ) {
+  return $('<div></div>').html('<i>' + message + '</i>');
+};
+
+var processUserInput = function( chatApp, socket ) {
+  var message = $('#send-message').val();
+  var systemMessage;
+
+  if ( message.chatAt( 0 ) === '/' ) {
+    systemMessage = chatApp.processCommand( message );
+
+    if ( systemMessage ) {
+      $('#message').append( divSystemContentElement( systemMessage ) );
+    } 
+  } else {
+    chatApp.sendMessage( $('#room').text(), message);
+
+    $('#messages').append( divEscapedContentElement( message ) );
+    $('#messages').scrollTop( $('#messages').prop( 'scrollHeight') );
+  }
+
+  $('#send-message').val('');
+
+};
